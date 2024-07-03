3ebf740cc78bdf690784151cb2decd0870b8c949
siddham-jain <siddhamjain224@gmail.com>
2024-07-03 11:06:04 +0530

    implemented CRUD on api

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+
 let courses = [
     {id: 1, name: "DSA"},
     {id: 2, name: "DBMS"},
@@ -14,4 +17,39 @@ app.get('/courses', (req, res) => {
 const port = 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
+});
+
+app.post('/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.json(course);
+});
+
+app.delete('/courses/:id', (req, res) => {
+    const courseId = courses.find(c => c.id === parseInt(req.params.id));
+    if (!courseId){
+        return res.status(404).send('The course with the given ID was not found.');
+    }
+
+    const index = courses.indexOf(courseId);
+    const deletedCourse = courses.splice(index, 1)[0];
+
+    // Update IDs of remaining courses
+    for (let i = index; i < courses.length; i++) {
+        courses[i].id = i + 1;
+    }
+    res.json({ deletedCourse, updatedCourses: courses });
+});
+
+app.put('/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course){
+        return res.status(404).send('The course with the given ID was not found.');
+    }
+
+    course.name = req.body.name;
+    res.json(course);
 });
