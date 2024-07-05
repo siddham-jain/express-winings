f5b4868e0fcab4010a51344605ca82e75567de93
siddham-jain <siddhamjain224@gmail.com>
2024-07-05 10:14:19 +0530

    Added support for mongo db

--- index.js
+++ index.js
@@ -91,3 +91,30 @@ app.put('/courses/:id', (req, res) => {
     fs.writeFileSync(coursesFilePath, JSON.stringify(courses));
     res.json(course);
 });
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://siddhamjain:<password>@mongo-winings.8i8fsyd.mongodb.net/?appName=mongo-winings";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
